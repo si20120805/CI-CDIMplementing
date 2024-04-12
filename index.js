a64447b5c1b99da838807c48723df63ebe63d4a4
siddharth <siddarathsinghkvb@gmail.com>
2024-04-13 00:14:46 +0530

    added get request

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const express= require("express");
 const app=express();
+app.get('/alllist',(req,res)=>{
+    res.json({msg:'success'})
+})
 app.listen(5000,()=>{console.log('listinging to the port ')})
