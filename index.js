473de851f91c3d2bf332db8576482fee0d5f11b0
siddharth <siddarathsinghkvb@gmail.com>
2024-04-23 23:49:53 +0530

    console.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ app.get('/alllist',(req,res)=>{
     res.json({msg:'success'})
 })
 app.get('/all',(req,res)=>{
+    console.log()
     res.json({msg:'to test the tag in the GIT'})
 })
 app.listen(5000,()=>{console.log('listinging to the port ')})
